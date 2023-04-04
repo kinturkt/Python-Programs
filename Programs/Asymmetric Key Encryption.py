e905f512f4c081170a1f4830c7436693e0afea12
Kintur Shah <73271742+kinturkt@users.noreply.github.com>
2023-04-04 16:44:55 +0530

    Add files via upload

---
+++ Programs/Asymmetric Key Encryption.py
@@ -0,0 +1,11 @@
+import rsa
+publicKey, privateKey = rsa.newkeys(512)
+message = "hello kintur"
+encMessage = rsa.encrypt(message.encode(), publicKey)
+
+print("original string: ", message)
+
+print("encrypted string: ", encMessage)
+decMessage = rsa.decrypt(encMessage, privateKey).decode()
+
+print("decrypted string: ", decMessage)
