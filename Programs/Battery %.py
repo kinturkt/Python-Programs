637bccff1568972abd8e9ee315250ab065ce3a42
Kintur Shah <73271742+kinturkt@users.noreply.github.com>
2023-04-04 16:41:44 +0530

    Delete Battery %.py

--- Programs/Battery %.py
+++
@@ -1,6 +0,0 @@
-import psutil
-
-battery=psutil.sensors_battery()
-percentage=battery.percent
-print("My current battery % is", percentage)
-print(f"My system has {percentage} percent battery left")
