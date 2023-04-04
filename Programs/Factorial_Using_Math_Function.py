1b6202fd6075d494c2ff676fea1b3578920609ac
Kintur Shah <73271742+kinturkt@users.noreply.github.com>
2023-04-04 16:42:38 +0530

    Add files via upload

---
+++ Programs/Factorial_Using_Math_Function.py
@@ -0,0 +1,5 @@
+import math
+def factorial(n):
+    return(math.factorial(n))
+a=int(input("Enter the number: "))
+print("Factorial of",a, "is",factorial(a))
---
+++ Programs/Fibonacci_Using_Forloop.py
@@ -0,0 +1,18 @@
+# Python Fibonacci series Program using For Loop
+
+# Fibonacci series will start at 0 and travel upto below number
+Number = int(input("\nPlease Enter the Range Number: "))
+
+# Initializing First and Second Values of a Series
+First_Value = 0
+Second_Value = 1
+           
+# Find & Displaying Fibonacci series
+for Num in range(0, Number):
+           if(Num <= 1):
+                      Next = Num
+           else:
+                      Next = First_Value + Second_Value
+                      First_Value = Second_Value
+                      Second_Value = Next
+           print(Next)
